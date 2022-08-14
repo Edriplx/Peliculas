b579bb387f274e4fe54796692cf9c3168f81329b
Edri Villagran <edripl31@gmail.com>
2022-08-14 16:15:36 -0500

    proyecto inicial

---
+++ view/actores/index.php
@@ -0,0 +1,7 @@
+<?php include 'header.php' ?>
+<!-- Main content -->
+<section class="content"> <!-- /.aqui empieza el dashboard para la programacion ------------------------------------------------------------->
+    Hi word
+</section>
+
+<?php include 'footer.php' ?>
